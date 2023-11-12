77ef51ca9992458c8692c2c18e5d443319137b54
anelka777 <yourclearvoice@gmail.com>
2023-11-12 10:16:55 -0800

    feat: add next few resolved tasks

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -350,8 +350,9 @@ function isPowerOfTwo(num) {
  *   0 => 0
  *   Math.PI / 2 => 1
  */
-function getSine(/* num */) {
-  throw new Error('Not implemented');
+function getSine(num) {
+  const sine = Math.sin(num);
+  return sine;
 }
 
 /**
@@ -365,8 +366,9 @@ function getSine(/* num */) {
  * 255, 16 => 'ff'
  * 2, 2    => '10'
  */
-function numberToStringInBase(/* number, base */) {
-  throw new Error('Not implemented');
+function numberToStringInBase(number, base) {
+  const stringRep = number.toString(base);
+  return stringRep;
 }
 
 /**
@@ -379,8 +381,9 @@ function numberToStringInBase(/* number, base */) {
  * @example:
  * 12345, 2    => '1.23e+4'
  */
-function toExponential(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toExponential(number, fractionDigits) {
+  const stringRep = number.toExponential(fractionDigits);
+  return stringRep;
 }
 
 /**
@@ -394,8 +397,9 @@ function toExponential(/* number, fractionDigits */) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(number, fractionDigits) {
+  const stringRep = number.toFixed(fractionDigits);
+  return stringRep;
 }
 
 /**
@@ -410,8 +414,9 @@ function toFixed(/* number, fractionDigits */) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
+function toPrecision(number, precision) {
+  const stringRep = number.toPrecision(precision);
+  return stringRep;
 }
 
 /**
@@ -424,8 +429,8 @@ function toPrecision(/* number, precision */) {
  * new Number(5) => 5
  * Number(-5)    => -5
  */
-function getNumberValue(/* number */) {
-  throw new Error('Not implemented');
+function getNumberValue(number) {
+  return number.valueOf();
 }
 
 /**
